ce87834ebdd6d8fb5d2f4bc046a8e8a03d47ff0d
chalbrik <paw.swa95@gmail.com>
2024-02-21 17:10:28 +0100

    Update changes

---
+++ index.js
@@ -0,0 +1,23 @@
+const incomesData = [
+  {
+    category: "Salary",
+    amount: 17.45,
+  },
+  {
+    category: "Bank interest",
+    amount: 34.91,
+  },
+  {
+    category: "Sales",
+    amount: 52.36,
+  },
+  {
+    category: "Investments",
+    amount: 31.07,
+  },
+  {
+    category: "Others",
+    amount: 23.39,
+  },
+];
+
