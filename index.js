f4fc9c5dd8732f2448e84e51591b1d66099cb941
chalbrik <paw.swa95@gmail.com>
2024-02-23 18:41:23 +0100

    Update changes

--- index.js
+++ index.js
@@ -33,3 +33,88 @@ document.querySelectorAll(".nav-item").forEach((navItem) => {
     navItem.querySelector("img.nav-icon").nextElementSibling.hidden = true;
   });
 });
+
+
+//doughnut - incomes
+const incomesCtx = document.getElementById('incomeDoughnutChart');
+
+new Chart(incomesCtx, {
+type:'doughnut',
+data:{
+  labels: [
+    'Salary',
+    'Bank interest',
+    'Sales',
+    'Investments',
+    'Other'
+  ],
+  datasets: [{
+    label: 'Incomes',
+    data: [4200, 580, 310, 308, 97],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)',
+      'rgb(255, 88, 86)',
+      'rgb(255, 115, 26)'
+    ],
+    hoverOffset: 4
+  }]
+},
+options: {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+      position: 'right',
+    },
+    title: {
+      display: false,
+      text: 'Chart.js Doughnut Chart'
+    }
+  }
+},
+
+});
+
+//doughnut - incomes
+const expensesCtx = document.getElementById('expensesDoughnutChart');
+
+new Chart(expensesCtx, {
+type:'doughnut',
+data:{
+  labels: [
+    'Salary',
+    'Bank interest',
+    'Sales',
+    'Investments',
+    'Other'
+  ],
+  datasets: [{
+    label: 'Expenses',
+    data: [2250, 340, 270, 102, 43],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)',
+      'rgb(255, 88, 86)',
+      'rgb(255, 115, 26)'
+    ],
+    hoverOffset: 4
+  }]
+},
+options: {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+      position: 'right',
+    },
+    title: {
+      display: false,
+      text: 'Chart.js Doughnut Chart'
+    }
+  }
+},
+
+});
