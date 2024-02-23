6fbd3039dae6cf04152eb933b98c641e26960405
chalbrik <paw.swa95@gmail.com>
2024-02-23 14:23:41 +0100

    Update changes

--- index.js
+++ index.js
@@ -21,3 +21,15 @@ const incomesData = [
   },
 ];
 
+//rozwijaj pełną nazwę linków w navbarze
+document.querySelectorAll(".nav-item").forEach((navItem) => {
+  navItem.addEventListener("mouseover", () => {
+    navItem.querySelector("img.nav-icon").nextElementSibling.hidden = false;
+  });
+});
+
+document.querySelectorAll(".nav-item").forEach((navItem) => {
+  navItem.addEventListener("mouseout", () => {
+    navItem.querySelector("img.nav-icon").nextElementSibling.hidden = true;
+  });
+});
